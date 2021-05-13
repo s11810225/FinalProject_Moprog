c458895b053276f7fe8bcedeaca4753ff2b5aaac
ChristofelN <73183415+ChristofelN@users.noreply.github.com>
2021-05-13 11:33:44 +0800

    Create index.js

---
+++ src/component/molecules/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Header
+ = () => {
+    return (
+        <View>
+            <Text>Header Component</Text>
+        </View>
+    )
+}
+
+export default Header
+
+
+const styles = StyleSheet.create({})
